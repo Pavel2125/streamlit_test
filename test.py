fad7a1df046fc76243b8dc57471015e50fa65ec0
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-01 15:07:10 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,52 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=[1,2,3,4,5]
-y=[5,4,4,3,2]
+#x=[1,2,3,4,5]
+#y=[5,4,4,3,2]
 #plt.plot(x,y)
-print(x)
+#print(x)
+
+
+import requests, os
+from gwpy.timeseries import TimeSeries
+from gwosc.locate import get_urls
+from gwosc import datasets
+from gwosc.api import fetch_event_json
+
+from copy import deepcopy
+import base64
+
+from helper import make_audio_file
+
+# Use the non-interactive Agg backend, which is recommended as a
+# thread-safe backend.
+# See https://matplotlib.org/3.3.2/faq/howto_faq.html#working-with-threads.
+import matplotlib as mpl
+mpl.use("agg")
+
+##############################################################################
+# Workaround for the limited multi-threading support in matplotlib.
+# Per the docs, we will avoid using `matplotlib.pyplot` for figures:
+# https://matplotlib.org/3.3.2/faq/howto_faq.html#how-to-use-matplotlib-in-a-web-application-server.
+# Moreover, we will guard all operations on the figure instances by the
+# class-level lock in the Agg backend.
+##############################################################################
+from matplotlib.backends.backend_agg import RendererAgg
+_lock = RendererAgg.lock
+
+
+# -- Set page config
+apptitle = 'GW Quickview'
+
+st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
+
+# -- Default detector list
+detectorlist = ['H1','L1', 'V1']
+
+# Title the app
+st.title('Gravitational Wave Quickview')
+
+st.markdown("""
+ * Use the menu at left to select data and set plot parameters
+ * Your plots will appear below
+""")
