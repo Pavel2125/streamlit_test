78fc7e23c93cf8347e1098d235382ced057abee9
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-01 18:10:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -226,13 +226,10 @@ with _lock:
     #st.pyplot(fig4, clear_figure=True)
     
     arr = np.random.normal(1, 1, size=100)
-
     fig4 = plt.figure()
-
     plt.hist(arr, bins=20)
-
-    st.plotly_chart(fig4)
-
+    #st.plotly_chart(fig4)
+    st.pyplot(fig4, clear_figure=True)
 
  
 
