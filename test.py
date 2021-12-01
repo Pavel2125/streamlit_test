6b8e767706440c91e837a82c7cb54aa3d5d0faef
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-01 15:42:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+
+
+echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
+import matplotlib.pyplot as plt
 
 #x=[1,2,3,4,5]
 #y=[5,4,4,3,2]
