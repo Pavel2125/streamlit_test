2cf31e0d4d9ef5d6e690d056c4016a76f9a5b9cb
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-07 13:30:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -95,7 +95,8 @@ with _lock:
     
      df = pd.DataFrame(
            np.random.randn(10, 5),
-           columns=('CuBe/TAV6', 'CuBe/Al', 'NiCrAl/Al'))
+#          columns=('CuBe/TAV6', 'CuBe/Al', 'NiCrAl/Al'))
+           columns=('col %d' % i for i in range(5)))
 st.table(df)    
   
 
