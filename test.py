0713f31d981498a00999ecb36a53a38a793d9c6a
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-10 14:37:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -181,11 +181,13 @@ st.table(df2)
 
 #st.pyplot(fig4, clear_figure=True)
 
-
-st.write('CuBe/TAV6 with analytical parameters')
-st.write('a=[4,8], P=8kbar, Signal=a^2*(np.exp(-A1*(c-a)))*(np.exp(-A2*(b-c)))')
-
 a=np.array([4,5,6,7,8]) 
+st.write('CuBe/TAV6 with analytical parameters')
+st.write('Inner diameter a=, mm',a)
+st.write('Maximal pressure, kbar', Pressure_max)
+st.write('Signal, Signal=a^2*(np.exp(-A1*(c-a)))*(np.exp(-A2*(b-c)))')
+
+
 #b=(c[0]**2)*np.sqrt((Sigma[0])/(Alfa[0]*(c[0]**2)*Sigma[0]+(c[0]**2)*Sigma[0]-(a**2)*Alfa[0]*Sigma[0]-2*Pressure_max*100*(c[0]**2)))
 
 
