6a38f4034dd1bcf9be23f090d924c1720c83dbb1
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-10 10:20:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -178,9 +178,9 @@ st.table(df2)
   
 
 
-    arr = np.random.normal(1, 1, size=100)
-    fig4 = plt.figure()
-    plt.hist(arr, bins=20)
+arr = np.random.normal(1, 1, size=100)
+fig4 = plt.figure()
+plt.hist(arr, bins=20)
    # #st.plotly_chart(fig4)
 
 st.pyplot(fig4, clear_figure=True)
