b2c2cf4be296143942017d44f3e5a3c63a24e732
Pavel Naumov <46075036+Pavel2125@users.noreply.github.com>
2021-12-10 14:42:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -181,14 +181,7 @@ st.table(df2)
 
 #st.pyplot(fig4, clear_figure=True)
 
-a=np.array([4,5,6,7,8]) 
-st.markdown('CuBe/TAV6 with analytical parameters')
-st.write('Inner diameter a=, mm',a)
-st.write('Maximal pressure, kbar', Pressure_max)
-st.write('Outer diameter', b)
-st.write('Signal=a^2*(np.exp(-A1*(c-a)))*(np.exp(-A2*(b-c)))')
-
-
+a=np.array([4,5,6,7,8])
 #b=(c[0]**2)*np.sqrt((Sigma[0])/(Alfa[0]*(c[0]**2)*Sigma[0]+(c[0]**2)*Sigma[0]-(a**2)*Alfa[0]*Sigma[0]-2*Pressure_max*100*(c[0]**2)))
 
 
@@ -197,6 +190,15 @@ C=(np.sqrt(np.sqrt(Alfa[0])))*np.sqrt(a*b)
 y=(a**2)*(np.exp(-Attenuation_length[0,1]*(C-a)))*(np.exp(-Attenuation_length[0,0]*(b-C)))
 
 #y=(a**2)*(np.exp(-Attenuation_length[0,1]*(c[0]-a)))*(np.exp(-Attenuation_length[0,0]*(b-c[0])))
+
+st.markdown('CuBe/TAV6 with analytical parameters')
+st.write('Inner diameter a=, mm',a)
+st.write('Maximal pressure, kbar', Pressure_max)
+st.write('Outer diameter b=(2*a*Sigma[0]*(np.sqrt(Alfa[0])))/(Sigma[0]*(1+Alfa[0])-2*Pressure_max*100)')
+st.write('Signal=a^2*(np.exp(-A1*(c-a)))*(np.exp(-A2*(b-c)))')
+
+
+
 
 fig5 = plt.figure()
 plt.plot(a,y)
